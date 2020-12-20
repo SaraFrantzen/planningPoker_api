c70b0a1e1d1aa7e7d12fbe3acc7a582e32fb0ba6
sara <saralundkvist86@gmail.com>
2020-12-20 14:36:32 +0100

    updates factory for poll to have association of user. Updates model and req specs. All test goes green

--- spec/request/api/polls/create_spec.rb
+++ spec/request/api/polls/create_spec.rb
@@ -35,24 +35,49 @@ RSpec.describe 'POST /api/polls', type: :request do
     end
   end
 
-  describe "without valid params" do
+  describe 'without valid params' do
     before do
-      post "/api/polls",
+      post '/api/polls',
            params: {
              poll: {
-               title: "API can provide polls index",
-             },
+               title: 'API can provide polls index'
+             }
            }, headers: headers
     end
 
-    it "responds with unprocessable entity status" do
+    it 'responds with unprocessable entity status' do
       expect(response).to have_http_status :unprocessable_entity
     end
 
-    it "returns a unsuccesfully message if params are blank" do
-      expect(response_json["message"]).to eq "Description can't be blank, Tasks can't be blank, and Points can't be blank"
+    it 'returns a unsuccesfully message if params are blank' do
+      expect(response_json['message']).to eq "Description can't be blank, Tasks can't be blank, and Points can't be blank"
+    end
+  end
+
+  describe 'unauthorized without credentials unsuccessfully create an poll' do
+    let(:user) { create(:user) }
+    let(:credentials) { user.create_new_auth_token }
+    let(:headers) { { HTTP_ACCEPT: 'application/json' } }
+
+    before do
+      post '/api/polls',
+           params: {
+             poll: {
+               title: 'API can provide polls index',
+               description: 'As an API.
+							In order to provide a list of polls in DB.
+							I would like to provide an index endpoint for polls.',
+               tasks: 'index action, routes to the action, polls model: title, description, polls Index in serializer',
+               points: []
+             }
+           }, headers: headers
+    end
+    it 'responds with unauthorized status' do
+      expect(response).to have_http_status :unauthorized
+    end
+
+    it 'returns error message' do
+      expect(response_json['errors'][0]).to eq 'You need to sign in or sign up before continuing.'
     end
   end
 end
-
-
