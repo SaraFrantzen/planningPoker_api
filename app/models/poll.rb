871d863ed914e49cd4d7a5e543d0fa032cb9a5cb
sara <saralundkvist86@gmail.com>
2020-12-21 02:33:36 +0100

    removes validation of precence for points in Poll model

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,4 +1,4 @@
 class Poll < ApplicationRecord
-  validates_presence_of :title, :description, :tasks, :points
+  validates_presence_of :title, :description, :tasks
   belongs_to :user, class_name: "User"
 end
