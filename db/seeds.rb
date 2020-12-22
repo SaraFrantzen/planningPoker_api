e3e8196fb5e51fad80b385b67baa687df653a267
sara <saralundkvist86@gmail.com>
2020-12-22 19:18:24 +0100

    formats seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,31 +7,21 @@ user2 = User.create(name: "Mattias", email: "mattias@epidemicSound.com", passwor
 
 poll1 = Poll.create(
   title: 'API can provide polls index',
-  description: 'As an API.
-      In order to provide a list of polls in DB.
-      I would like to provide an index endpoint for polls.',
-  tasks: '
-  Index action. Routes to the action. Polls model: title, description. Polls Index in serializer'
-  
+  description: 'As an API. In order to provide a list of polls in DB. I would like to provide an index endpoint for polls.',
+  tasks: 'Index action. Routes to the action. Polls model: title, description. Polls Index in serializer'
 )
 poll2 = Poll.create(
   title: 'Visitor can see list of polls',
-  description: 'As a visitor.
-      In order to see what polls there are.
-      I would like to see the polls listed on a page.',
+  description: 'As a visitor. In order to see what polls there are. I would like to see the polls listed on a page.',
   tasks: 'GET request. Display polls'
 )
 poll3 = Poll.create(
   title: 'API provides abaility for users to create polls',
-  description: 'As an API.
-      In order to allow users to login & create polls.
-      I would like to provide functionality to sign in & an endpoint to create action.',
+  description: 'As an API. In order to allow users to login & create polls. I would like to provide functionality to sign in & an endpoint to create action.',
   tasks: 'Create User model. Devise token auth. Create action. Associations poll-user'
 )
 poll4 = Poll.create(
   title: 'User can create polls',
-  description: 'As a user.
-      In order to make my polls public.
-      I would like to be able to post my polls.',
+  description: 'As a user. In order to make my polls public. I would like to be able to post my polls.',
   tasks: 'Create-new-poll button -> login filter. Sign In form. J-tok Auth. Create-poll form. POST req'
 )
