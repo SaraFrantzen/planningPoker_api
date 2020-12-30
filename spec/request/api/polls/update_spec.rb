862d85eb65320969d060ec187e5dcb6a418f4c19
sara <saralundkvist86@gmail.com>
2020-12-30 07:17:25 +0100

    formats spec

--- spec/request/api/polls/update_spec.rb
+++ spec/request/api/polls/update_spec.rb
@@ -5,96 +5,7 @@ RSpec.describe 'PUT /api/polls', type: :request do
   let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials) }
   let!(:user1) { create(:user, email: 're-vote_user@mail.com') }
 
-  
-  describe 'user successfully un-vote on a poll' do
-    before do
-      put "/api/polls/#{poll.id}",
-          params: {
-            poll: {
-              points: 3,
-              votes: { "#{user.uid}": "#{poll.points}"}
-            }
-          }, headers: headers
-    end
-    before do
-      put "/api/polls/#{poll.id}",
-          params: {
-            poll: {
-              points: 3,
-              votes: { "#{user.uid}": "#{poll.points}"}
-            }
-          }, headers: headers
-    end
-
-    it 'responds with ok status' do
-      expect(response).to have_http_status :ok
-    end
-
-    it 'returns success message' do
-      expect(response_json['message']).to eq 'successfully un-voted'
-    end
-
-    it 'updates an poll with votes' do
-      poll = Poll.last
-      expect(poll.votes).to eq :"votingUser1@mail.com"=>0, :"votingUser2@mail.com"=>2
-    end
-  end
-  
-  
-  
-  
-  
-  
-  
-  
-  describe 'user successfully vote on a poll' do
-    before do
-      put "/api/polls/#{poll.id}",
-          params: {
-            poll: {
-              points: 3,
-              votes: { "#{user.uid}": "#{poll.points}"}
-            }
-          }, headers: headers
-    end
-
-    it 'responds with ok status' do
-      expect(response).to have_http_status :ok
-    end
-
-    it 'returns success message' do
-      expect(response_json['message']).to eq 'successfully voted'
-    end
-
-    it 'updates an poll with points' do
-      poll = Poll.last
-      expect(poll.points).to eq [2, 3]
-    end
-
-    it 'updates an poll with votes' do
-      poll = Poll.last
-      expect(poll.votes).to eq :"votingUser2@mail.com"=>2, :"votingUser1@mail.com"=>0,"#{user.uid}"=>"#{3}"
-    end
-  end
-
-  describe 'unsuccessfully join - not authorized' do
-    before do
-      put "/api/polls/#{poll.id}",
-          params: {
-            poll: {
-              points: 3,
-              votes: { "#{user.uid}": "#{poll.points}"}
-            }
-          }
-    end
-
-    it 'responds with unauthorized status' do
-      expect(response).to have_http_status :unauthorized
-    end
-  end
-
-
-  describe 'user successfully apply to join a poll' do
+  describe 'user successfully join a poll' do
     before do
       put "/api/polls/#{poll.id}",
           params: {
@@ -139,6 +50,84 @@ RSpec.describe 'PUT /api/polls', type: :request do
       expect(response_json['message']).to eq 'You already joined this poll'
     end
   end
+
+  describe 'user successfully vote on a poll' do
+    before do
+      put "/api/polls/#{poll.id}",
+          params: {
+            poll: {
+              points: 3,
+              votes: { "#{user.uid}": poll.points.to_s }
+            }
+          }, headers: headers
+    end
+
+    it 'responds with ok status' do
+      expect(response).to have_http_status :ok
+    end
+
+    it 'returns success message' do
+      expect(response_json['message']).to eq 'successfully voted'
+    end
+
+    it 'updates an poll with points' do
+      poll = Poll.last
+      expect(poll.points).to eq [2, 3]
+    end
+
+    it 'updates an poll with votes' do
+      poll = Poll.last
+      expect(poll.votes).to eq :"votingUser2@mail.com" => 2, :"votingUser1@mail.com" => 0, user.uid.to_s => '3'
+    end
+  end
+
+  describe 'unsuccessfully vote - not authorized' do
+    before do
+      put "/api/polls/#{poll.id}",
+          params: {
+            poll: {
+              points: 3,
+              votes: { "#{user.uid}": poll.points.to_s}
+            }
+          }
+    end
+
+    it 'responds with unauthorized status' do
+      expect(response).to have_http_status :unauthorized
+    end
+  end
+
+  describe 'user successfully un-vote on a poll' do
+    before do
+      put "/api/polls/#{poll.id}",
+          params: {
+            poll: {
+              points: 3,
+              votes: { "#{user.uid}": poll.points.to_s}
+            }
+          }, headers: headers
+    end
+    before do
+      put "/api/polls/#{poll.id}",
+          params: {
+            poll: {
+              points: 3,
+              votes: { "#{user.uid}": poll.points.to_s}
+            }
+          }, headers: headers
+    end
+
+    it 'responds with ok status' do
+      expect(response).to have_http_status :ok
+    end
+
+    it 'returns success message' do
+      expect(response_json['message']).to eq 'successfully un-voted'
+    end
+
+    it 'updates an poll with votes' do
+      poll = Poll.last
+      expect(poll.votes).to eq "votingUser1@mail.com": 0, "votingUser2@mail.com": 2
+    end
+  end
 end
-
-
