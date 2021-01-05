801bbdc6bbdaf85e464435495907b905b3f93fe0
sara <saralundkvist86@gmail.com>
2021-01-05 19:41:51 +0100

    creates spec for show comments, updates the routes with :show

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth', skip: [:omniauth_callbacks]
   namespace :api do
     resources :polls, only: [:index, :create, :show, :update]
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :show]
   end
 end
---
+++ spec/request/api/comments/show_spec.rb
@@ -0,0 +1,29 @@
+RSpec.describe 'GET /api/comments', type: :request do
+  let(:user) { create(:user) }
+  let(:credentials) { user.create_new_auth_token }
+  let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials) }
+  let(:poll) { create(:poll) }
+  let!(:comment) do
+    5.times do
+      create(:comment, poll_id: poll.id)
+    end
+  end
+
+  describe 'visitor can see specific poll' do
+    before do
+      get "/api/comments/#{poll.id}"
+    end
+
+    it 'responds with ok status' do
+      expect(response).to have_http_status :ok
+    end
+
+    it 'returns a specific comment' do
+      expect(response_json['comments'].first['comment']).to eq 'MyComment'
+    end
+
+    it 'returns 5 comments' do
+      expect(response_json['comments'].count).to eq 5
+    end
+  end
+end
